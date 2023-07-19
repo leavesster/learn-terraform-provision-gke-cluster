609ca25768cecc345aaf6b2e667f1fec38de9de4
yleaf <11785335+leavesster@users.noreply.github.com>
2023-07-19 16:05:34 +0800

    use autopilot mode

--- gke.tf
+++ gke.tf
@@ -21,41 +21,46 @@ resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
   location = var.region
   
+  enable_autopilot = true
+  # https://github.com/hashicorp/terraform-provider-google/issues/10782#issuecomment-1080195853
+  ip_allocation_policy {
+  }
+
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 1
+#   remove_default_node_pool = true
+#   initial_node_count       = 1
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
 }
 
 # Separately Managed Node Pool
-resource "google_container_node_pool" "primary_nodes" {
-  name       = google_container_cluster.primary.name
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
-  node_count = var.gke_num_nodes
+# resource "google_container_node_pool" "primary_nodes" {
+#   name       = google_container_cluster.primary.name
+#   location   = var.region
+#   cluster    = google_container_cluster.primary.name
+#   node_count = var.gke_num_nodes
 
-  node_config {
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+#   node_config {
+#     oauth_scopes = [
+#       "https://www.googleapis.com/auth/logging.write",
+#       "https://www.googleapis.com/auth/monitoring",
+#     ]
 
-    labels = {
-      env = var.project_id
-    }
+#     labels = {
+#       env = var.project_id
+#     }
 
-    # preemptible  = true
-    machine_type = "n1-standard-1"
-    tags         = ["gke-node", "${var.project_id}-gke"]
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-  }
-}
+#     # preemptible  = true
+#     machine_type = "n1-standard-1"
+#     tags         = ["gke-node", "${var.project_id}-gke"]
+#     metadata = {
+#       disable-legacy-endpoints = "true"
+#     }
+#   }
+# }
 
 
 # # Kubernetes provider
