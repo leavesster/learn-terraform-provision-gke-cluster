134361961705204a7ef945167c7aecf307e6f28e
yleaf <11785335+leavesster@users.noreply.github.com>
2023-09-21 15:34:13 +0800

    update jumper server

--- instance.tf
+++ instance.tf
@@ -1,15 +1,15 @@
 # https://cloud.google.com/compute/docs/images?hl=zh-cn#os-compute-support
 # gcloud compute images list --project ubuntu-os-cloud
 data "google_compute_image" "ubuntu" {
-   family = "ubuntu-2204-lts"
-    project = "ubuntu-os-cloud"
+  family  = "ubuntu-2204-lts"
+  project = "ubuntu-os-cloud"
 }
 
 resource "google_compute_instance" "machine" {
-  name = "gke-control-panel"
+  name         = "gke-control-panel"
   machine_type = "f1-micro"
-  zone = "${var.region}-a"
-  tags = ["ssh"]
+  zone         = "${var.region}-a"
+  tags         = ["jump"]
   boot_disk {
     initialize_params {
       image = data.google_compute_image.ubuntu.self_link
@@ -17,21 +17,28 @@ resource "google_compute_instance" "machine" {
   }
   network_interface {
     access_config {
-
     }
+    network    = google_compute_network.vpc.name
+    subnetwork = google_compute_subnetwork.subnet.name
+
+  }
+
+  metadata = {
+    ssh_keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
+
   }
 }
 
 resource "google_compute_firewall" "ssh" {
-    name = "allow-ssh"
-    allow {
-      ports = ["22"]
-      protocol = "tcp"
-    }
+  name = "allow-kube-jump-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
 
-    direction = "INGRESS"
-    network = google_compute_network.vpc.id
-    priority = 1000
-    source_ranges = ["0.0.0.0/0"]
-    target_tags = ["ssh"]
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["jump"]
 }
